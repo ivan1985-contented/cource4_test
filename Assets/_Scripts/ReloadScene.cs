72fe7c83398d04f7c66225c95ab834b2340e088f
ivan85m <head6@tut.by>
2019-11-02 22:45:33 +0300

    Test task for course 4

---
+++ Assets/_Scripts/ReloadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadScene : MonoBehaviour
+{
+    public string sceneName;
+    public void LoadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
